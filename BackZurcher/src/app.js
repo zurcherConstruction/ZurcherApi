1c437eb080d46e1f04869a3742dc48bc3fc0c07d
mlobeto <mercedeslobeto@gmail.com>
2025-03-31 12:08:57 -0300

    io

--- BackZurcher/src/app.js
+++ BackZurcher/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app); // Crear el servidor HTTP
 const io = new Server(server, {
   cors: {
-    origin: 'https://zurcher-api-9526.vercel.app', // Cambia esto según el dominio de tu frontend
+    origin: '*', // Cambia esto según el dominio de tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
