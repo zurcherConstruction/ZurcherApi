b449adf6daad2f16d59495fe5142e741fde8c457
mlobeto <mercedeslobeto@gmail.com>
2025-04-17 17:39:50 -0300

    uploadInitialInvoice

---
+++ FrontZurcher/src/Components/Budget/UploadInitialInvoice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    fetchBudgets,
+    updateBudget,
+    uploadInvoice,
+  } from "../../Redux/Actions/budgetActions";
+  import { useDispatch, useSelector } from "react-redux";
+
+  
+const UploadInitialInvoice = () => {
+  return (
+    <div>UploadInitialInvoice</div>
+  )
+}
+
+export default UploadInitialInvoice
