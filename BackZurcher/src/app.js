eea66df2ecab7dc921ac50d984bd4faacc0b6f74
mlobeto <mercedeslobeto@gmail.com>
2025-04-19 10:52:58 -0300

    cambio ruta io

--- BackZurcher/src/app.js
+++ BackZurcher/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app); // Crear el servidor HTTP
 const io = new Server(server, {
   cors: {
-    origin: '*', // Cambia esto según el dominio de tu frontend
+    origin: 'https://zurcher-api.vercel.app', // Cambia esto según el dominio de tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
@@ -64,7 +64,7 @@ app.use(passport.initialize());
 
 // Session
 app.use(cors({
-  origin: '*', // Permitir cualquier origen
+  origin: 'https://zurcher-api.vercel.app', // Permitir cualquier origen
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
   credentials: true, // Permitir el uso de credenciales
