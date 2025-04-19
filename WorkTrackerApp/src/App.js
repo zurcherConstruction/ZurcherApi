1aa30fd2772344a3d1a2f89b319c80fd86cde163
yanirc1981 <cerzurc@hotmail.com>
2025-04-18 22:55:24 -0300

    notificaciones en back y logout

--- WorkTrackerApp/src/App.js
+++ WorkTrackerApp/src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './Redux/store';
 import MainNavigator from './navigations/MainNavigator'; // Usar siempre MainNavigator
-import '../global.css'
+import '../global.css';
 import Toast from "react-native-toast-message";
+import { NotificationProvider } from './utils/notificationContext';
+
+const AppContent = () => {
+  const { isAuthenticated, staff } = useSelector((state) => state.auth);
+  const staffId = staff?.id;
+
+  console.log('AppContent mounted with staffId:', staffId);
+
+  // Mostrar un indicador de carga si el usuario está autenticado pero el staffId aún no está disponible
+  if (isAuthenticated && !staffId) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <NotificationProvider staffId={staffId}>
+          <MainNavigator />
+        </NotificationProvider>
+      ) : (
+        <MainNavigator />
+      )}
+    </>
+  );
+};
+
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MainNavigator /> 
+      <AppContent />
       <Toast />
     </Provider>
-     
   );
 }
