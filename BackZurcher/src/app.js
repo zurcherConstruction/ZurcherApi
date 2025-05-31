945753145bccbcf4fc2bae9c5fd63f775883250f
mlobeto <mercedeslobeto@gmail.com>
2025-05-30 23:54:22 -0300

    comente auth en servee

--- BackZurcher/src/app.js
+++ BackZurcher/src/app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/', routes);
-app.use('/auth', authRoutes);
+//app.use('/auth', authRoutes);
 
 // Not Found Middleware
 app.use('*', (req, res) => {
