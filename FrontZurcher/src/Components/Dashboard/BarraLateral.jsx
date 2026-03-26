ae572185bdad2fa87ab329ccee3d5e122eb4171f
yanirc1981 <cerzurc@hotmail.com>
2026-03-26 00:54:16 -0300

    probando redeploy de vercel

--- FrontZurcher/src/Components/Dashboard/BarraLateral.jsx
+++ FrontZurcher/src/Components/Dashboard/BarraLateral.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; //probando redeploy
 import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
